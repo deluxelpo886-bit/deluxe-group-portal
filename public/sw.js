9395db9d4f53a34f827561347656fdb5fb2e7e63
deluxelpo886-bit <deluxelpo886@gmail.com>
2026-07-21 09:48:32 +0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,8 +5,8 @@ var CACHE_NAME = 'deluxe-portal-shell-v1';
 var SHELL_FILES = [
   '/',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', function(event){
